048fd12bed96837f1c88370cb9643e25a3956cb1
André Peixoto <opastortsapa@gmail.com>
2024-05-10 05:31:10 -0300

    Adicionei Usuario e Senha
    Troquei as cores tbm

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function substituir() {
     var frase00 = document.getElementById("frase0"); // Atribui uma variavel para a frase padrão em HTML
-    var rand = ["NÃO ESQUECER A SENHA!", "NOOBs"];     // Criar lista com novas frases a serem usadas
+    var rand = ["<b>NÃO ESQUECER A SENHA!</b>", "<b>NOOBs</b>"];     // Criar lista com novas frases a serem usadas
     var indice = Math.floor(Math.random() * rand.length); // Criando alternador randomico (função math * quantia de itens na lista rand)
-    frase00.textContent = rand[indice];
+    frase00.innerHTML = rand[indice];
 }
 
-setInterval(substituir, 3000);
+setInterval(substituir, 2000); // Função do JS de repetição (2000 = 2 segundos aprox para alterar frase)
+
+// setInterval é uma função em JavaScript. Ela é uma função integrada à linguagem JavaScript que é usada para repetir a execução de uma função ou a avaliação de uma expressão em intervalos de tempo regulares.
