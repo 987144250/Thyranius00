a098b1fa87082e8015107b03835462f9e71f4167
André Peixoto <opastortsapa@gmail.com>
2024-05-17 05:05:44 -0300

    Update Fucking 01
    Criei img de logo
    Editei imagens
    Criei pagina inicial
    Criei as 3 janelas e testei
    Noob: hidden quando + zoom

---
+++ pags/css/inicio.js
@@ -0,0 +1,9 @@
+function aumentarjanela(element) { // usa-se 'element' nos parenteses quando não houver variavel atribuida com div de box especifica;
+    element.style.height = "200px" // tambem usa-se 'element' no inicio ao inves do nome da variável ou document.
+}
+
+function diminuirjanela(element) {
+    element.style.height = "100px" // Janela volta para tamanho original;
+    
+}
+
--- script.js
+++ script.js
@@ -8,3 +8,5 @@ function substituir() {
 setInterval(substituir, 2000); // Função do JS de repetição (2000 = 2 segundos aprox para alterar frase)
 
 // setInterval é uma função em JavaScript. Ela é uma função integrada à linguagem JavaScript que é usada para repetir a execução de uma função ou a avaliação de uma expressão em intervalos de tempo regulares.
+
+
