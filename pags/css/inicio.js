f25522aa39e7244977d3d32c8c58a5a7ae34197f
André Peixoto <opastortsapa@gmail.com>
2024-05-25 05:28:43 -0300

    update na pag-inicio + inicio Mobile

--- pags/css/inicio.js
+++ pags/css/inicio.js
@@ -1,9 +1,47 @@
+
+
 function aumentarjanela(element) { // usa-se 'element' nos parenteses quando não houver variavel atribuida com div de box especifica;
-    element.style.height = "200px" // tambem usa-se 'element' no inicio ao inves do nome da variável ou document.
+    
+    if (element.classList.contains('box-manuais')) { // use o classList.contains(nome da janela) para criar condições dentro dela.
+                                                     // essa em específico teve a ver com a altura do box-manuais ser maior q os demais.
+        element.style.height = "300px"
+    } else {
+        element.style.height = "230px"
+    }
+    
+    setTimeout(function() {
+        // Mostra o texto com transição
+  
+    if (element.classList.contains('box-imagens')) { // use o classList.contains(nome da janela) para criar condições dentro dela(surgir)
+        element.querySelector('.lista-box01').style.opacity = "1";
+        element.querySelector('.lista-box01').style.display = 'block'
+    } else if (element.classList.contains('box-livros')) {
+        element.querySelector('.lista-box02').style.opacity = "1";
+        element.querySelector('.lista-box02').style.display = 'block'
+    } else if (element.classList.contains('box-manuais')) {
+        element.querySelector('.lista-box03').style.opacity = "1";
+        element.querySelector('.lista-box03').style.display = 'block'
+       
+    }
+
+}, 50); // Velocidade de janela expandindo
 }
 
 function diminuirjanela(element) {
-    element.style.height = "100px" // Janela volta para tamanho original;
+    element.style.height = "100px"; // Janela volta para o tamanho original
     
+    // Mostra o texto com transição
+    setTimeout(function() {
+        if (element.classList.contains('box-imagens')) { // use o classList.contains(nome da janela) para criar condições dentro dela.
+            element.querySelector('.lista-box01').style.display = 'none'; // Desativa a exibição na tela
+        } else if (element.classList.contains('box-livros')) {
+            element.querySelector('.lista-box02').style.display = 'none'; // Desativa a exibição na tela
+        } else if (element.classList.contains('box-manuais')) {
+            element.querySelector('.lista-box03').style.display = 'none'; // Desativa a exibição na tela
+            
+        }
+        
+       
+       // Mostra o texto com transição
+    }, 50);
 }
-
